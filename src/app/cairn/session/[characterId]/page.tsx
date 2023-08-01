6fe39d9aa96b58efaea75215a69b00ee910878c3
Simon Bergot <sbergot@cegid.com>
2023-08-01 17:31:30 +0200

    wip cairn

--- src/app/cairn/session/[characterId]/page.tsx
+++ src/app/cairn/session/[characterId]/page.tsx
@@ -7,61 +7,55 @@ import {
   usePlayerConnectionContext,
 } from "../../cairn-context";
 import { Button } from "@/components/ui/button";
-import { uuidv4 } from "@/lib/utils";
 import {
   MessagePanel,
   ShowCustomMessageProps,
 } from "@/components/generic-pages/message-panel";
-import { Message } from "@/lib/game/cairn/types";
+import { AbilityType, Message, RollMode } from "@/lib/game/cairn/types";
 import { abilityCheck } from "@/lib/game/cairn/utils";
 import { DiceRoll } from "@/components/ui/dice-roll";
 import { Title } from "@/components/ui/typography";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { DicesIcon } from "lucide-react";
+import { useState } from "react";
 
 function CharacterSheet() {
-  const { character, setCharacter } = useCharacterStorage();
-  const { log } = usePlayerConnectionContext();
+  const { character } = useCharacterStorage();
 
   return (
     <div className="flex flex-col gap-4 max-w-sm">
       <Title>Attributes</Title>
       <div className="flex gap-8">
         <div className="max-w-min">
-          <Ability name="Strength" value={character.strength} />
-          <Ability name="Dexterity" value={character.dexterity} />
-          <Ability name="Willpower" value={character.willpower} />
+          <AbilityControl type="strength" />
+          <AbilityControl type="dexterity" />
+          <AbilityControl type="willpower" />
         </div>
         <div className="max-w-min">
           <Ability name="HP" value={character.hp} />
           <Field name="Armor">0</Field>
         </div>
-        <Button
-          onClick={() => {
-            log({
-              kind: "chat-common",
-              type: "SimpleMessage",
-              props: { content: uuidv4() },
-            });
-          }}
-        >
-          common
-        </Button>
-        <Button
-          onClick={() => {
-            log({
-              kind: "chat-custom",
-              type: "AbilityRoll",
-              title: "dexterity check",
-              props: abilityCheck({
-                abilityName: "dexterity",
-                abilityValue: character.dexterity.current,
-                mode: "advantage",
-              }),
-            });
-          }}
-        >
-          custom
-        </Button>
       </div>
+    </div>
+  );
+}
+
+interface AbilityControlProps {
+  type: AbilityType;
+}
+
+function AbilityControl({ type }: AbilityControlProps) {
+  const { character } = useCharacterStorage();
+  return (
+    <div className="flex items-center gap-2 justify-between w-full">
+      <Ability name={type} value={character[type]} />
+      <AbilityCheckModal type={type} />
     </div>
   );
 }
@@ -96,5 +90,49 @@ export default function Session() {
         />
       }
     />
+  );
+}
+
+interface AbilityCheckModalProps {
+  type: AbilityType;
+}
+
+export function AbilityCheckModal({ type }: AbilityCheckModalProps) {
+  const { character } = useCharacterStorage();
+  const { log } = usePlayerConnectionContext();
+  const [open, setOpen] = useState(false);
+
+  function roll(mode: RollMode) {
+    log({
+      kind: "chat-custom",
+      type: "AbilityRoll",
+      title: type + " check",
+      props: abilityCheck({
+        abilityName: type,
+        abilityValue: character[type].current,
+        mode,
+      }),
+    });
+    setOpen(false);
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button size="icon-sm" variant="ghost">
+          <DicesIcon size={20} />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>{type} check</DialogTitle>
+          <div className="flex gap-1">
+            <Button onClick={() => roll("normal")}>normal</Button>
+            <Button onClick={() => roll("advantage")}>advantage</Button>
+            <Button onClick={() => roll("disadvantage")}>disadvantage</Button>
+          </div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
