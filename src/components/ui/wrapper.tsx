fa0a9dece650bf34caaf623a15fc7d21c320073c
Simon Bergot <sbergot@cegid.com>
2023-08-21 11:07:01 +0200

    try to fix build

--- src/components/ui/wrapper.tsx
+++ src/components/ui/wrapper.tsx
@@ -1,11 +1,9 @@
-import { useEffect } from "react";
 import { Children } from "./types";
 
 export default function Wrapper({ children }: Children) {
-  useEffect(() => {
-    if (global.navigator === undefined) {
-      global.navigator = { clipboard: {} as any } as any;
-    }
-  }, []);
+  if (global.navigator === undefined) {
+    global.navigator = { clipboard: {} as any } as any;
+  }
+
   return <>{children}</>;
 }
