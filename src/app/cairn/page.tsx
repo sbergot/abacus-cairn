ec5aff5b3568f8a14043d20c155ba3d35f2acaed
Simon Bergot <sbergot@cegid.com>
2023-08-21 10:53:27 +0200

    try to fix build

--- src/app/cairn/page.tsx
+++ src/app/cairn/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import MainMenu from "@/components/generic-pages/main-menu";
 import { CairnCharacter } from "@/lib/game/cairn/types";
 import { BaseGame } from "@/lib/game/types";
--- src/components/ui/wrapper.tsx
+++ src/components/ui/wrapper.tsx
@@ -1,5 +1,8 @@
 import { Children } from "./types";
 
 export default function Wrapper({ children }: Children) {
+  if (window === undefined) {
+    return <div />
+  }
   return <>{children}</>;
 }
